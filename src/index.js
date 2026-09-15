2f42e89c4d37641d46989e93057b90ca32d637b2
Priyank Sondagar <priyank.s@moontechnolabs.com>
2026-09-15 13:03:43 +0530

    fix: greeting message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 function getMessage() {
-    return "Hello Users: This is a new release";
-  }
+  return "Hello Users, This is a new release";
+}
 
-  function getMessage2() {
-    return "Hello Users: This is a new release 2";
-  }
-
-  console.log(getMessage());
-  console.log(getMessage2());
+console.log(getMessage());
